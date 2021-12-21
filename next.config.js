a55d546d8d0dc4af46b1cb83a578b344178fb69d
Archie Smyth <archiesmyth26@googlemail.com>
2021-12-21 16:50:55 +0000

    next.config.js - added next/images

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
+const withImages = require('next-images')
 module.exports = {
+  images: {
+    domains: ["images.unsplash.com", "ipfs.io/ipfs/", "ipfs.io"],
+  },
   reactStrictMode: true,
-  basePath: '',
-}
+  basePath: "",
+};
+
+module.exports = withImages()
