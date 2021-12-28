6c75d15716b244a08ff2ec73a4cbf43c8c434141
Archie Smyth <archiesmyth26@googlemail.com>
2021-12-28 19:54:57 +0000

    update heading style on experience and contact

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -7,7 +7,7 @@ const Contain: NextPage = () => {
   return (
     <div className='flex items-center justify-center' >
      <div>
-       <h1 className='text-center text-6xl text-emerald-400 font-title'> How you can get in contact</h1>
+       <h1 className='text-center text-xl md:text-4xl lg:text-5xl xl:text-8xl text-emerald-400 font-title'> How you can get in contact</h1>
      </div>
     </div>
   )
--- pages/experience.tsx
+++ pages/experience.tsx
@@ -4,7 +4,7 @@ const Experience: NextPage = () => {
   return (
     <div className='flex items-center justify-center' >
      <div>
-       <h1 className='text-center text-6xl text-emerald-400 font-title'> Experience!</h1>
+       <h1 className='text-center text-xl md:text-4xl lg:text-5xl xl:text-8xl text-emerald-400 font-title'> Experience!</h1>
      </div>
     </div>
   )
