d6c1d0aee311e4e0400022b7f8674830f2b8e2b2
Archie Smyth <archiesmyth26@googlemail.com>
2021-12-28 19:54:27 +0000

    added interface model for Project from CMS

---
+++ interfaces/projects/index.ts
@@ -0,0 +1,9 @@
+export interface IProject {
+  projectSlug: string;
+  projectName: string;
+  projectDescription: string;
+  projectFeatures: string[];
+  projectTools: string[];
+  projectTags: string[]
+  projectURLs: string[];
+}
