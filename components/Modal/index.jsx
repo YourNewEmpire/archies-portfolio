2359580071b43c44ac3979662d5944ce79401e48
Archie Smyth <archiesmyth26@googlemail.com>
2022-01-07 14:44:55 +0000

    added Modal + Backdrop components

---
+++ components/Modal/index.jsx
@@ -0,0 +1,53 @@
+import { motion } from "framer-motion";
+import Backdrop from "../Backdrop";
+
+const dropIn = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      type: "spring",
+      damping: 25,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+  },
+};
+
+const Modal = ({ handleClose, children }) => {
+  return (
+    <Backdrop onClick={handleClose}>
+      <motion.div
+        onClick={(e) => e.stopPropagation()}
+        className="rounded-lg bg-slate-700 ring-2 ring-emerald-300 p-6"
+        variants={dropIn}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+      >
+        {children}
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          onClick={handleClose}
+          className="
+            w-64 m-12 rounded-2xl font-semibold
+          bg-emerald-300 text-slate-700 
+            "
+        >
+          CLOSE
+        </motion.button>
+      </motion.div>
+    </Backdrop>
+  );
+};
+
+export default Modal;
