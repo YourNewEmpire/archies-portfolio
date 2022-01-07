561d1e6ef2c4a1f915bee15567118ab5a3198790
Archie Smyth <archiesmyth26@googlemail.com>
2022-01-07 15:29:44 +0000

    Contact - added ContactButtons to jsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,14 +1,11 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
+import ContactButtons from '../components/ContactForm/ContactButtons'
 
 const Contain: NextPage = () => {
   return (
-    <div className='flex items-center justify-center' >
-     <div>
-       <h1 className='text-center text-xl md:text-4xl lg:text-5xl xl:text-8xl text-emerald-400 font-title'> How you can get in contact</h1>
-     </div>
+    <div className='flex flex-col items-center justify-center' >
+     <ContactButtons/>
     </div>
   )
 }
