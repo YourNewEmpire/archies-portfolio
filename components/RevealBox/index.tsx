78ae07cb292792fbbfa185a93daa61fcdc9bde64
Archie Smyth <archiesmyth26@googlemail.com>
2022-01-07 14:47:26 +0000

    Reveal Box component added

---
+++ components/RevealBox/index.tsx
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { Childs } from "../../interfaces/children";
+export interface IRevealBox extends Childs {}
+const RevealBox = ({ children }: IRevealBox) => {
+  const animationControl = useAnimation();
+  const { inView, entry, ref } = useInView();
+  useEffect(() => {
+    if (inView) {
+      animationControl.start({
+        opacity: 1,
+        x: 0,
+        y: 0,
+        transition: {
+          delay: 0.1,
+          duration: 1.1,
+        },
+      });
+    }
+  }, [inView]);
+  return (
+    <div ref={ref}>
+      <motion.div
+        initial={{
+          opacity: 0,
+        }}
+        animate={animationControl}
+      >
+        {" "}
+        {children}
+      </motion.div>
+    </div>
+  );
+};
+export default RevealBox;
