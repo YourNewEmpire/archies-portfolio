2b25d9f2a59d57068fc3a5f3a694eaf3abb16287
Archie Smyth <archiesmyth26@googlemail.com>
2022-01-09 17:00:01 +0000

    about page - polished skills + added experience

--- pages/aboutme.tsx
+++ pages/aboutme.tsx
@@ -1,19 +1,12 @@
 import type { NextPage } from "next";
 import Image from "next/image";
-import {
-  FaReact,
-  FaDatabase,
-  FaEthereum,
-  FaHtml5,
-  FaNodeJs,
-} from "react-icons/fa";
-import { SiWeb3Dotjs } from "react-icons/si";
+import skillList from "../lib/content/about/skills";
 import RevealBox from "../components/RevealBox";
-
-
+import certs from "../lib/content/about/certs";
+import MoreDetails from "../components/Buttons/MoreDetails";
+import { FaExternalLinkAlt } from "react-icons/fa";
 const AboutMe: NextPage = () => {
 
-//todo - add items to lib
   return (
     <>
       <div className="flex flex-col items-center justify-start min-h-screen">
@@ -45,11 +38,19 @@ const AboutMe: NextPage = () => {
               Full Stack Web3 Developer
             </p>
           </div>
-          <div className="col-span-1 xl:col-span-8 font-body text-emerald-300 text-xl xl:text-3xl text-center flex flex-col items-center justify-center">
-            Hi, I am Archie, currently working on personal projects to fulfill
-            my ambition on becoming a full-stack web3 developer. Scroll to find
-            out more about my skills, experience, qualifications, interests and
-            more.
+          <div
+            className="col-span-1 xl:col-span-8 font-body 
+          text-emerald-300 text-xl xl:text-3xl text-center 
+          flex flex-col items-center justify-center
+       
+          "
+          >
+            <p>
+              Hi, I am Archie, currently working on personal projects to fulfill
+              my ambition on becoming a full-stack web3 developer. Scroll to
+              find out more about my skills, experience, qualifications,
+              interests and more.
+            </p>
           </div>
         </div>
       </div>
@@ -64,68 +65,52 @@ const AboutMe: NextPage = () => {
         </RevealBox>
         <RevealBox>
           <div className="grid grid-flow-row grid-cols-3 gap-4 lg:gap-10 xl:gap-24 p-2 xl:p-12 items-center justify-center ">
-            <div className="flex flex-col justify-center items-center space-y-2 ">
-              <FaReact className="text-emerald-300 w-24 h-24 xl:w-40 xl:h-40" />
-              <h1 className="text-lg xl:text-4xl text-emerald-300 font-semibold">
-                React
-              </h1>
-              <p className="text-emerald-500 text-xs xl:text-base">
-                Next.js, CRA, React-Router + TypeScript
-              </p>
-            </div>
-            <div className="flex flex-col justify-center items-center space-y-2 ">
-              <FaHtml5 className="text-emerald-300 w-24 h-24 xl:w-40 xl:h-40" />
-              <h1 className="text-lg xl:text-4xl text-emerald-300 font-semibold">
-                HTML and CSS
-              </h1>
-              <p className="text-emerald-500 text-xs xl:text-base">
-                Tailwind CSS or Material UI + The DOM
-              </p>
-            </div>
-            <div className="flex flex-col justify-center items-center space-y-2 ">
-              <FaNodeJs className="text-emerald-300 w-24 h-24 xl:w-40 xl:h-40" />
-              <h1 className="text-lg xl:text-4xl text-emerald-300 font-semibold">
-                Node JS
-              </h1>
-              <p className="text-emerald-500  text-xs xl:text-base">
-                Server-side APIs + Hardhat scripts and tests
-              </p>
-            </div>
-            <div className="flex flex-col justify-center items-center space-y-2 ">
-              <FaDatabase className="text-emerald-300 w-24 h-24 xl:w-40 xl:h-40" />
-              <h1 className="text-lg xl:text-4xl text-emerald-300 font-semibold">
-                <Image
-                  src={"/Moralis_Logo/SVG/iconlight.svg"}
-                  alt="moralis-icon"
-                  width={25}
-                  height={25}
-                />{" "}
-                Moralis
-              </h1>
-              <p className="text-emerald-500 text-xs xl:text-base">
-                DB, Web3API + Business logic in the cloud
-              </p>
-            </div>
-            <div className="flex flex-col justify-center items-center space-y-2 ">
-              <FaEthereum className="text-emerald-300 w-24 h-24 xl:w-40 xl:h-40" />
-              <h1 className="text-lg xl:text-4xl text-emerald-300 font-semibold">
-                Solidity/EVM
-              </h1>
-              <p className="text-emerald-500  text-xs xl:text-base">
-                Smart contract security, EVM chains, OpenZeppelin contracts,
-                Hardhat + Ethers.js
-              </p>
-            </div>
-
-            <div className="flex flex-col justify-center items-center space-y-2 ">
-              <SiWeb3Dotjs className="text-emerald-300 w-24 h-24 xl:w-40 xl:h-40" />
-              <h1 className="text-lg xl:text-4xl text-emerald-300 font-semibold">
-                Ethers.JS / Web3.JS
-              </h1>
-              <p className="text-emerald-500 text-xs xl:text-base">
-                Hardhat scripts + front end contract interactions
-              </p>
-            </div>
+            {skillList.map((skill, index) => (
+              <div
+                key={index}
+                className="flex flex-col justify-center items-center space-y-2 "
+              >
+                {skill.icon}
+                <h1 className="text-lg xl:text-4xl text-emerald-300 font-semibold">
+                  {skill.name}
+                </h1>
+                <p className="text-emerald-500 text-xs xl:text-base">
+                  {skill.desc}
+                </p>
+              </div>
+            ))}
+          </div>
+        </RevealBox>
+        <RevealBox>
+          <div className="m-12">
+            <h1 className="text-xl md:text-4xl lg:text-5xl xl:text-8xl text-emerald-300 font-title">
+              {" "}
+              Certifications
+            </h1>
+          </div>
+        </RevealBox>
+        <RevealBox>
+          <div className="flex flex-col justify-center items-center space-y-8 xl:space-y-12">
+            <ol className="list-disc list-inside text-lg xl:text-2xl text-emerald-300 text-left grid grid-flow-row grid-cols-1 xl:grid-cols-3 gap-8">
+              {certs.map((cert, index) => (
+                <li key={index}>
+                  <a
+                    className="underline hover:no-underline"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={cert.link}
+                  >
+                    {cert.name}
+                  </a>
+                </li>
+              ))}
+            </ol>
+            <MoreDetails
+              text="View All on Google Drive"
+              href={
+                "https://drive.google.com/drive/folders/1mA4y17s9IqFUFDr3V1OYaq5gPBrxUsBV?usp=sharing"
+              }
+            />
           </div>
         </RevealBox>
       </div>
